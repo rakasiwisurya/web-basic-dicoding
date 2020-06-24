916b216239306f02c2bb3a8d73d6b0b79b9e7db0
rakasiwisurya <rakasiwi.surya@gmail.com>
2020-06-24 16:59:29 +0700

    Membuat dom javascript progress not completed

--- Submision 1/assets/script.js
+++ Submision 1/assets/script.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const menuIcon = document.querySelector('.menu-icon');
+const headerList = document.querySelectorAll('.header-list ul li');
+
+menuIcon.addEventListener('click', () => {
+    headerList.style.display = 'inline-block';
+    headerList.style.backgroundColor = 'red';
+    // headerList.style.zIndex = '11';
+    // headerList.style.position = 'relative';
+    // headerList.style.top = '50px';
+});
